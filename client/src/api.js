9473e3a05b8f57c089fa390b9f056a62931f7518
Sandeep Dheeravath <sunnynaik002@gmail.com>
2025-08-04 10:38:01 +0530

    rec

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
 // API Base URL - Update this for deployment
+<<<<<<< HEAD
 const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://mini-linkedin-backend-7jhs.onrender.com';
+=======
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5175/';
+>>>>>>> a2eb312 (recent)
 
 const API = axios.create({ baseURL: API_BASE_URL });
 
